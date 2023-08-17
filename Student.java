61d1a26d5c72e14578fb60f24a941e417edf4066
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-17 17:44:04 +0900

    設計図クラス定義

---
+++ Student.java
@@ -0,0 +1,22 @@
+class Student {
+    // メンバ変数（属性）
+    String name;
+    int engScore;
+    int mathScore;
+
+    // メソッド（操作）
+    void display() {
+        System.out.println(name + "さん");
+        System.out.println("英語" + engScore + "点・数学" + mathScore + "点");;
+    }
+
+    void setScore(int eng, int math){
+        engScore = eng;
+        mathScore = math;
+    }
+
+    double getAvg() {
+        double avg = (engScore + mathScore) / 2.0;
+        return avg;
+    }
+}
