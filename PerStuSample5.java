5d52d3972bc6217ba003f57dbcef28e46ceef0b1
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-06 20:44:47 +0900

    実行クラス50％完成

--- PerStuSample5.java
+++ PerStuSample5.java
@@ -1,5 +1,7 @@
 public class PerStuSample5 {
     public static void main(String[] args) {
-        
+        StudentPerson5 stu1 = new StudentPerson5("菅原", 1);
+        Person5 psn = stu1; //子クラスの中にある親クラス
+        psn.display(); //呼び出されるのはStudentPerson5.display()
     }
 }
