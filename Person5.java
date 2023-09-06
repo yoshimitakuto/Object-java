d4a732d609f073014997a548593e330fae61318b
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-06 13:16:56 +0900

    設計図クラス定義完了

--- Person5.java
+++ Person5.java
@@ -1,3 +1,14 @@
 public class Person5 {
-    
+    // メンバ変数
+    private String name;
+
+    // コンストラクタ
+    public Person5(String name) {
+        this.name = name;
+    };
+    // メソッド
+    public void display() {
+        System.out.println("名前：" + name);
+    };
+
 }
--- StudentPerson5.java
+++ StudentPerson5.java
@@ -1,3 +1,19 @@
 public class StudentPerson5 extends Person5 {
-    
+    // メンバ変数
+    private int stuNo;
+
+    // コンストラクタ
+    public StudentPerson5(String name, int stuNo) {
+        super(name);
+        this.stuNo = stuNo;
+    };
+    // メソッド（オーバーライド）
+    public void display() {
+        super.display();
+        System.out.println("学籍番号：" + stuNo);
+    };
+    // メソッド（独自）
+    public void chgStuNo(int stuNo) {
+        this.stuNo = stuNo;
+    };
 }
