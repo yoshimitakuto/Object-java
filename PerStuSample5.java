007556392321ff4f1f3efec538d1259a54ac7c63
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-06 23:27:57 +0900

    実行クラス定義

--- PerStuSample5.java
+++ PerStuSample5.java
@@ -3,5 +3,11 @@ public class PerStuSample5 {
         StudentPerson5 stu1 = new StudentPerson5("菅原", 1);
         Person5 psn = stu1; //子クラスの中にある親クラス
         psn.display(); //呼び出されるのはStudentPerson5.display()
+
+        if (psn instanceof StudentPerson5) {
+            StudentPerson5 stu2 = (StudentPerson5)psn;
+            stu2.chgStuNo(2);
+            stu2.display();
+        }
     }
 }
